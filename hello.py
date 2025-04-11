8ca1f7a924c0d551131080eda895b671ef683207
hschoi <capswar1@naver.net>
2025-04-11 09:37:35 +0900

    hello.py modified on local repository

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
+// for command line git tutorial
+// local repository modification
 print("Hello world")
 print("Tell your world")
-<<<<<<< HEAD
 print("Tell his world")
-=======
 print("Tell her world")
->>>>>>> hotfix
+
+
